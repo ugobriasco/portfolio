42fa785bca96cbe125fb191c21d42458dc3725c8
Ugo Briasco <ugo.briasco@gmail.com>
2017-05-24 22:59:44 +0200

    cleanup

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
 
   constructor(private http: Http, private twitter: TwitterService, private jsonp: Jsonp) { }
 
-  cfgUrl ='./docs/config.json'
+  
   resumeUrl = './docs/resume.json';
   portfolioUrl = './docs/portfolio.json';
 
@@ -23,42 +23,5 @@ export class ApiService {
     let a = this.http.get(this.portfolioUrl);
     return a;
   }
-
-  getTweets(){
-  	return this.twitter.get(
-          'https://api.twitter.com/1.1/statuses/user_timeline.json',
-          {
-            count: 10
-          },
-          {
-            consumerKey: "kHig3xUC2hZzgYCsHnynnFOY7",
-            consumerSecret: "NiJBQHoiK6laQvpDpMUqmdXV4RxoAQZ5R3YUljW3DAKn3p0coZ"
-          },
-          {
-            token: "341947997-HBTkX3B33KeV2Jn8h0BbGIEGphJe5wiTi9lxlHCj",
-            tokenSecret: "gsPYvYGOHljq6Tdb86XjjxXhacBE503y7325prSgtR4fG"
-          }
-    );
-  }
-
-
-//http://stackoverflow.com/questions/36510337/calling-twitter-api-from-postman-works-but-from-angular-2-app-it-doesnt
-
- _getTweets(){
-
-    var _url = "https://api.twitter.com/1.1/statuses/user_timeline.json";    
-    var headers = new Headers ({ 
-      
-      'Authorization': 'OAuth oauth_consumer_key="6bKoszKhLCqE4WGUWlnxncGkt", oauth_nonce="SyM1tCQyR5", oauth_signature="Sga8awsbZeefoID9C2KruaoeTyY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1494883130", oauth_token="341947997-makcDnZsyDUaRDx41UrLmbV0sjxEVswvkAVFfOBn", oauth_version="1.0"'
-
-    });
-    //return this.jsonp.get(_url,{headers: headers});
-    return this.http.get(_url, {headers: headers});
-                     
-                      
-  }
- 
-
-
 
 }
